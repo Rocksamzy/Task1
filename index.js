1366eef27fe69a77a0111e2aeb090a5ca469db84
Rocksamzy <rocksamzy4real@gmail.com>
2024-07-05 11:45:40 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
+const request = require("request");
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/api/hello.html', (req, res) => {
-    const visitorName = req.query.visitor_name;
-    const clientIp = req.ip; // This will get to the client's IP address
-    const location = "New York"; // Static location for demonstration purposes
-    const greeting = `Hello, ${visitorName}!, the temperature is 11 degreees Celsius in New York`;
-
-    res.json({
-        client_ip: clientIp,
-        location: location,
-        greeting: greeting
-    });
+app.get('/', (req, res) => {
+  let name = req.query.name;
+  let city = req.query.city;
+  var request = require("request");
+  request(
+    `https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=1b5ceba9a67f413a72aa8d1b700b92f0`,
+    function (error, response, body) {
+        let data = JSON.parse(body);
+      if (response.statusCode === 200) {
+        res.send(
+          `Hello! ${name}, the weather in your city "${city}" is ${data.list[0].weather[0].description}`
+        );
+      }
+    }
+  );
 });
 
-app.listen(3000, () => {
-    console.log(`Server running on http://localhost:${3000}`);
-});
+app.listen(5000, () => 
+  console.log(`Server running on port 5000`)
+);
